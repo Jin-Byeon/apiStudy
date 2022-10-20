2396f284c4df49fb0880d7ab56f74bdb6a2f3f9f
Jin Byeon <jin.byeon@wellysis.com>
2022-10-20 19:27:48 +0900

    Refactor: response return json data

--- src/main/java/com/study/apiStudy/controller/ArticleController.java
+++ src/main/java/com/study/apiStudy/controller/ArticleController.java
@@ -1,12 +1,10 @@
 package com.study.apiStudy.controller;
 
-import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.study.apiStudy.dto.ArticleDto;
 import com.study.apiStudy.service.ArticleService;
@@ -18,23 +16,8 @@ public class ArticleController {
 	ArticleService articleService;
 	
 	@RequestMapping(value = "/articles", method = RequestMethod.POST)
-	public ModelAndView CreateArticle(ArticleDto articleDto) {
-		ArticleDto article = articleService.articleRegister(articleDto);
-		
-		ModelAndView modelAndView = new ModelAndView();
-		
-		modelAndView.addObject("slug", article.getSlug());
-		modelAndView.addObject("title", article.getTitle());
-		modelAndView.addObject("description", article.getDescription());
-		modelAndView.addObject("body", article.getBody());
-		modelAndView.addObject("tagList", Arrays.deepToString(article.getTagList()));
-		modelAndView.addObject("createdAt", article.getCreatedAt());
-		modelAndView.addObject("updatedAt", article.getUpdatedAt());
-		modelAndView.addObject("favorited", article.isFavorited());
-		modelAndView.addObject("favoritesCount", article.getFavoritesCount());
-		modelAndView.setViewName("createArticle");
-		
-		return modelAndView;
+	@ResponseBody
+	public ArticleDto createArticle(ArticleDto articleDto) {
+		return articleService.articleRegister(articleDto);
 	}
-	
 }
